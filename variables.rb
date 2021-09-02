3919b88a9e5fd12e95bb68869d62ae4982a190ce
Darren Kulback <dkulback@gmail.com>
2021-09-02 11:27:31 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Shining"
+is_entertaining = true
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["Indian", "Mexican", "Thai"]
+favorite_foods.shift
+puts movie.length
+ 
