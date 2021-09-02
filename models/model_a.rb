6c0e4f38ec86bcea25005cebf6a79aae0eeee8d9
Darren Kulback <dkulback@gmail.com>
2021-09-02 11:36:21 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "Type" => "T-Rex",
+  "Color" => "Green",
+  "Teeth" => 10000,
+  "Bones" => 900,
+  "Alive" => false,
+  "Hair" => false,
+  "Origin" => ["Americas", "Europe", "Asia"],
+  "diet" => ["fish", "birds", "rabbits"]
+}
