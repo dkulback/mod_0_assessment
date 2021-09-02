c444f321a32dcf35f170f3bd852d265f5d37f70e
Darren Kulback <dkulback@gmail.com>
2021-09-02 11:43:45 -0600

    Add developer hash model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Bill"
+  "Height" => "5'11",
+  "Age" => 27,
+  "Graduated" => 2001,
+  "Employed" => true,
+  "Backend" => false,
+  "Programs" => ["Ruby", "Javascript", "C++"],
+  "Learning" => ["Python", "Java"]
+}
